2664251d7b2260048a9faf05f696ab5a4fd6c234
Conan Kunhwan Ahn <kunhwon@gmail.com>
2020-01-14 23:19:08 +0900

    Webpack Ouput Management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import _ from 'lodash';
 
+import preintMe from './print.js';
+
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  btn.innerHTML = 'Click me and dcheck the console!';
+  btn.onclick = preintMe;
+
+  element.appendChild(btn);
 
   return element;
 }
